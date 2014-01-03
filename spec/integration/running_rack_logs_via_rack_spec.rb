2cef0ac1e951474ebc6dac27028b13a6a2f3cd07
Jon Rowe <hello@jonrowe.co.uk>
2014-01-03 15:46:54 +1100

    basic behaviour

--- spec/integration/running_rack_logs_via_rack_spec.rb
+++ spec/integration/running_rack_logs_via_rack_spec.rb
@@ -16,4 +16,30 @@ describe 'running `Rack::Logs` via `Rack::Builder`', type: :integration do
       expect(last_response.body).to eq ''
     end
   end
+
+  context 'given a log in a configured directory' do
+    let(:app) do
+      Rack::Builder.app do
+        logs = Rack::Logs.configure do |config|
+          config.log_dir = './tmp'
+        end
+        run logs
+      end
+    end
+
+    before do
+      File.open('./tmp/my_log_file.log','w') do |f|
+        f.write "LOG ENTRY 1234"
+      end
+      get '/'
+    end
+
+    example 'returns a 200 response code' do
+      expect(last_response).to be_ok
+    end
+
+    example 'accessing the log returns log contents' do
+      expect(last_response.body).to match 'LOG ENTRY 1234'
+    end
+  end
 end
